f5eee2619aac141000bc350170b62deae4c5c69d
Harry <78512759+henry9960@users.noreply.github.com>
2023-06-14 17:46:12 +0100

    Fixed bug.
    Fixed the space that appeared between letters.

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ chars = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '_', 'a', 'b', '
          'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
          'u', 'v', 'w', 'x', 'y', 'z']
 
-fileOutput = open('usernamesDiscord.txt', 'w')
+fileOutput = open('usernamesDiscord.txt', 'a')
 # Outputs a file containing the randomised usernames in the same directory as the script.
 
 for x in range(1000):  # Adjust the range for the amount of usernames to print.
-    print('h', random.choice(chars), random.choice(chars), file=fileOutput)
+    print('h', random.choice(chars), random.choice(chars), file=fileOutput, sep='')
     # Adjust the print for a specific char in a specific spot.
+    # Sep makes it so the letters aren't separated.
